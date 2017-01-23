836f70a6fa9a8141cda066e8762d7897fb0783e1
w_cyn <wasonga.cynthia@yahoo.in>
2017-01-23 21:46:07 +0000

    Authenticate to Firebase
    User sign up and Login functioning. Not yet intergrated with FLask

---
+++ app/auth.py
@@ -0,0 +1,33 @@
+import pyrebase
+
+relevance_threshold = 0.5
+config = {
+	"apiKey": "AIzaSyDzBN-pfGvMGR1aIsjTkXEehavEN1TDZMs",
+    "authDomain": "psqair.firebaseapp.com",
+    "databaseURL": "https://psqair.firebaseio.com",
+    "storageBucket": "psqair.appspot.com",
+    "messagingSenderId": "470726324781"
+
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+
+auth = firebase.auth()
+email = "wasonga.cynthia@yahoo.in"
+password = "12345678"
+# auth.create_user_with_email_and_password(email, password)
+
+user = auth.sign_in_with_email_and_password(email, password)
+db = firebase.database()
+
+data = {
+    "name": "Mortimer 'Morty' Smith 2"
+}
+results = db.child("pair_sessions").push(data, user['idToken'])
+# pair_sessions = db.child("pair_sessions").get()
+
+# for p_session in pair_sessions.each():
+#     print(p_session.key())
+#     print(p_session.val())
